8227438e5d07d9a41a602ab32410dc65013db035
rishichandran1417 <rishichandran17@gmail.com>
2025-07-29 15:39:21 +0530

    Update: Center logo and adjust leaderboard spacing

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import "./navbar.css"; // Make sure this path is correct
-
+import "./navbar.css"; 
 export default function Navbar() {
   return (
     <nav className="navbar">
